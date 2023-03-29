accefb408aa589a2aaf39d1b5dca6362027c35a9
danicaon <danica_escot@hotmail.com>
2023-03-28 21:12:17 -0500

    adding template on dev necessary used

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
