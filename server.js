9a8c0018aaccbc00810fa8dde783d7e6812fae0e
danicaon <danica_escot@hotmail.com>
2023-03-30 20:38:48 -0500

    Adding comments

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+//my dependencies
 const express = require('express');
 const path = require('path');
-const fs = require("fs");
-const util = require ("util");
 
+//Express App
 const app = express();
 const PORT = 3001;
 
+//For data parsing
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.json());//For data parsing json
+app.use(express.static("public")); //middleware static files from public
+//HTML and API routes for the page
 require("./Develop/routes/apiRoute")(app);
 require("./Develop/routes/htmlRoutes")(app);
 
+//server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
